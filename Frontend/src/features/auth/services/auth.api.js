e358c051e5272e21c93e775961a31ae34cb81246
SohailShaikh7860 <sohailshaikh6417@gmail.com>
2026-05-16 23:47:24 +0530

    add axios login, logout, and profile functions

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -12,3 +12,34 @@ export async function registerUser({ username, email, password }) {
     console.log(error);
   }
 }
+
+
+export async function loginUser({email, password}) {
+  try {
+    const response = await instance.post('/login', {
+      email,
+      password,
+    })
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function logOutUser(){
+   try {
+     const response = await instance.get('/logout');
+      return response.data;
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+export async function getUserProfile() {
+  try {
+     const response = await instance.get('/profile');
+     return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
