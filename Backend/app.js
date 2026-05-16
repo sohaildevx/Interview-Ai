bda64edb21c8ec771f2bba33ab08bd2a7beb8904
SohailShaikh7860 <sohailshaikh6417@gmail.com>
2026-05-17 00:24:40 +0530

    add cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import authRouter from './src/routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.use('/auth', authRouter);
 
